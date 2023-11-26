4d01073506d4348e98a6a7c40ee1d3b7e1975f94
SShogun <sohamshirke.215099@gmail.com>
2023-11-26 12:34:03 +0530

    Update JS

---
+++ script.js
@@ -0,0 +1,32 @@
+const quotesData = [
+    {
+        quote: 'The only way to do great work is to love what you do'
+    }, {
+        quote: 'The difference between ordinary and extraordinary is that little extra'
+    }, {
+        quote: 'Do not go gentle into that good night, rage, rage against the dying of the light'
+    }, {
+        quote: 'The greatest glory in living lies not in never falling, but in rising everytime we fall'
+    }, {
+        quote: 'The only way to avoid doing great work is to try to avoid work all together'
+    }, {
+        quote: 'The best way to predict the future is to create it'
+    }, {
+        quote: 'The only limit to our realization of tomorrow will be our doubts of today'
+    }, {
+        quote: 'The only person you are destined to become is the person you decide to be'
+    }, {
+        quote: 'The only thing we have to fear is fear itself'
+    }, {
+        quote: 'The only constant in life is change'
+    }
+]
+
+//* get all the constants
+const quoteText = document.getElementById("quoteText");
+
+loadQuotes();
+
+function loadQuotes() {
+    quoteText.textContent = 'This is a quote';
+}
